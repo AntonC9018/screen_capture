dd005875f4de69f4a50384d7431155878b71546b
AntonC9018 <anton.curm@gmail.com>
2022-01-31 20:25:12 +0200

    input

--- source/app.d
+++ source/app.d
@@ -27,6 +27,18 @@ void main()
     //         style);
     // }();
 
+    // Ideally this should buffer, but I don't write much UTF-16, so at this point I don't care.
+    static HANDLE hConsole;
+    static void writew(const(wchar)[] buffer)
+    {
+        while (buffer.length > 0)
+        {
+            DWORD written;
+            WriteConsoleW(hConsole, buffer.ptr, cast(DWORD) buffer.length, &written, null);
+            buffer = buffer[written .. $];
+        }
+    }
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
     {
         HWND[] goodWindows;
@@ -50,18 +62,52 @@ void main()
             return 1;
         }
 
-        foreachWindow(&CrashOnThrow!foreachFunc);
-
-
+        while (true)
         {
-            foreach (handle; goodWindows)
+            goodWindows.length = 0;
+            foreachWindow(&CrashOnThrow!foreachFunc);
+            
+            foreach (size_t index, handle; goodWindows[])
             {   
                 enum int maxBufferLength = 512;
                 wchar[maxBufferLength] textBuffer;
                 GetWindowText(handle, textBuffer.ptr, maxBufferLength); 
 
-                import std.string : fromStringz;
-                writeln("Inspecting window: ", fromStringz(textBuffer.ptr));
+                import std.string;
+                import std.algorithm;
+                import std.utf;
+
+                write(index, ": ");
+                writew(fromStringz(textBuffer.ptr));
+                writeln();
+            }
+
+            enum ShortCircuitInputs
+            {
+                success = 0, // Returned if the int parsed.
+                requery = 1, // Command to requery windows.
+                close = 2,   // Close the app.
+            }
+
+            alias promptIntInRange = Prompt!(128).intInRange!(ShortCircuitInputs);
+            
+            const result = promptIntInRange(0, cast(int) goodWindows.length - 1);
+
+            final switch (result.type)
+            {
+                case ShortCircuitInputs.success:
+                {
+                    writeln("Selected window: ", result.value);
+                    break;
+                }
+                case ShortCircuitInputs.close:
+                {
+                    return;
+                }
+                case ShortCircuitInputs.requery:
+                {
+                    continue;
+                }
             }
         }
     }
@@ -83,7 +129,6 @@ template CrashOnThrow(alias func)
                 import core.stdc.stdlib;
                 exit(-1);
             }
-            return typeof(func(things)).init;
         }
     }
 }
@@ -105,4 +150,104 @@ BOOL foreachWindow(scope BOOL delegate(HWND) nothrow func)
     //     [in] LPARAM      lParam
     // );
     return EnumWindows(&wrapper, cast(LPARAM) &context);
+}
+
+
+template Prompt(size_t maxBufferSize)
+{
+    import std.string;
+
+    static auto tryConvertInputToIntInRange(const(char)[] input, int start, int end)
+    {
+        static struct Result
+        {
+            int value;
+            bool success;
+        }
+
+        int index;
+
+        import std.conv;
+        
+        try index = input.to!int;
+        catch (ConvOverflowException e)
+        {
+            writeln("Number overflowed. It must be contained within [", start, ", ", end, "].");
+            return Result(0, false);
+        }
+        catch (ConvException e)
+        {
+            writeln("The given input string is not a number.");
+            return Result(0, false);
+        }
+
+        if (index < start)
+        {
+            writeln("The number is too small. It must be contained within [", start, ", ", end, "].");
+            return Result(0, false);
+        }
+
+        if (index > end)
+        {
+            writeln("The number is too large. It must be contained within [", start, ", ", end, "].");
+            return Result(0, false);
+        }
+
+        return Result(index, true);
+    }
+
+    char[] getMeaningfulUserInput(char[] buffer)
+    {
+        while (true)
+        {
+            char[] input = buffer;
+            readln(input);
+            input = input.strip();
+            if (input.length > 0)
+                return input;
+        }
+    }
+
+    auto intInRange(ShortCircuitInputEnum, ShortCircuitInputEnum successMember = ShortCircuitInputEnum.success)
+        (int start, int end)
+    {
+        static struct Result
+        {
+            int value;
+            ShortCircuitInputEnum type;
+        }
+        // static assert(is(typeof(ShortCircuitInputEnum) == enum));
+        do
+        {
+            char[maxBufferSize] buffer;
+            char[] input = getMeaningfulUserInput(buffer[]);
+
+            static foreach (field; __traits(allMembers, ShortCircuitInputEnum))
+            {
+                static if (__traits(getMember, ShortCircuitInputEnum, field) != successMember)
+                if (input == field)
+                {
+                    return Result(0, __traits(getMember, ShortCircuitInputEnum, field));
+                }
+            }
+
+            const result = tryConvertInputToIntInRange(input, start, end);
+            if (result.success)
+                return Result(result.value, successMember);
+        }
+        while (true);
+    }
+
+    int intInRange(int start, int end)
+    {
+        do
+        {
+            char[maxBufferSize] buffer;
+            char[] input = getMeaningfulUserInput(buffer[]);
+            const result = tryConvertInputToIntInRange(input, start, end);
+            if (result.success)
+                return result.value;
+        }
+        while (true);
+    }
 }
